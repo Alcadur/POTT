b05c43904efa94f9927509ea30f5503e5da95131
Piotr Ch <chelmicki.p@gmail.com>
2023-02-19 14:58:26 +0100

    add ItemModel

---
+++ app/src/main/java/pl/hamsterdev/pott/ItemModel.kt
@@ -0,0 +1,29 @@
+package pl.hamsterdev.pott
+
+import org.json.JSONObject
+
+class ItemModel {
+    var id: String? = null
+    var name: String
+    var quantity: Int = 0
+    var expireAt: Long = 0
+
+    constructor(id: String? = null, name: String, quantity: Int, expireAt: Long) {
+        this.id = id
+        this.name = name
+        this.quantity = quantity
+        this.expireAt = expireAt
+    }
+
+    constructor(jsonString: String) {
+        val json = JSONObject(jsonString)
+        this.id = json.get("id").toString()
+        this.name = json.get("name").toString()
+        this.quantity = json.get("quantity").toString().toInt()
+        this.expireAt = json.get("expireAt").toString().toLong()
+    }
+
+    override fun toString(): String {
+        return "{ id: $id, name: $name, quantity: $quantity, expireAt: $expireAt }"
+    }
+}
