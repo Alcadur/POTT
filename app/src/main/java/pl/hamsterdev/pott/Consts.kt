43480b7478f5ba639327ccda37bac2c526af5160
Piotr Ch <chelmicki.p@gmail.com>
2023-02-22 21:59:33 +0100

    Fix null issue in notification message

--- app/src/main/java/pl/hamsterdev/pott/Consts.kt
+++ app/src/main/java/pl/hamsterdev/pott/Consts.kt
@@ -3,7 +3,7 @@ package pl.hamsterdev.pott
 object Consts {
     const val EXPIRE_NOTIFICATION_ITEM_NAME_PARAM = "itemName"
     const val EXPIRE_NOTIFICATION_ITEM_QUANTITY_PARAM = "itemQuantity"
-    const val EXPIRE_NOTIFICATION_DAYS_LEFT_PARAM = "itemDaysLeft"
+    const val EXPIRE_NOTIFICATION_ITEM_DAYS_LEFT_PARAM = "itemDaysLeft"
 
     const val EXPIRE_NOTIFICATION_WORKER_DAYS_BEFORE_EXPIRE = -8
     const val EXPIRE_NOTIFICATION_WORKER_HOUR_OF_DAY = 10
--- app/src/main/java/pl/hamsterdev/pott/ReminderWorker.kt
+++ app/src/main/java/pl/hamsterdev/pott/ReminderWorker.kt
@@ -9,7 +9,7 @@ class ReminderWorker(val context: Context, val params: WorkerParameters) : Worke
     override fun doWork(): Result {
         val itemName = inputData.getString(Consts.EXPIRE_NOTIFICATION_ITEM_NAME_PARAM)
         val itemAmount = inputData.getString(Consts.EXPIRE_NOTIFICATION_ITEM_QUANTITY_PARAM)
-        val daysLeft = inputData.getString(Consts.EXPIRE_NOTIFICATION_DAYS_LEFT_PARAM)
+        val daysLeft = inputData.getString(Consts.EXPIRE_NOTIFICATION_ITEM_DAYS_LEFT_PARAM)
 
         val title = context.resources.getString(R.string.item_expire_notification_title)
         val message = context.resources.getString(R.string.item_expire_notification_body, itemName, itemAmount, daysLeft)
